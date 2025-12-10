ef66aaf1ed3d804bbf6eb56d0f3e3cbf815545c0
yuvrajpanwar1611-lgtm <yuvrajpanwar1611@gmail.com>
2025-12-10 10:27:47 +0530

    changed frontend url

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ app = FastAPI(title="Inventory Management API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["https://inventory-management-frontend-hqhs.onrender.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
