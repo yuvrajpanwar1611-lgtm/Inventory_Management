4fa8f38be81c08e6929991b338555f22fc79085f
yuvrajpanwar1611-lgtm <yuvrajpanwar1611@gmail.com>
2025-12-14 21:27:28 +0530

    cors change

--- app.py
+++ app.py
@@ -671,23 +671,20 @@ from models import (
 #                 APP + CORS CONFIG
 # ========================================================
 
-app = FastAPI(title="Inventory Management API")
 
-origins = [
-    "*",
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "https://inventory-management-frontend-hqhs.onrender.com",
-]
+app = FastAPI(title="Inventory Management API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://inventory-management-frontend-hqhs.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 @app.get("/")
 def home():
     return {"msg": "API Running"}
